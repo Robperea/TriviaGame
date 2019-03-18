eae1d10652ad59a2169c5d422816189f8aada568
Rob13 <robperea@gmail.com>
2019-03-18 14:45:13 -0400

    portfolio commit

--- assets/app.js
+++ assets/app.js
@@ -14,9 +14,10 @@ $(document).ready(function(){
     var decrement = function () {
         number--;
         $("#quiz-timer").html("" + number + "");
-    }
-    if (number === 0) {
-        answers();
+    
+        if (number === 0) {
+            answers();
+        }
     }
 
   var startPage = function () {
@@ -39,12 +40,14 @@ $(document).ready(function(){
     
 
 var answers = function () {
-    var q1 = $("#q1:input:radio:checked").val();
+    var q1 = $("input[name=Q1]:checked").val();
+    console.log("Q1", q1);
+
 
     if (q1 === "greywind") {
         correct++;
         $("#correct").text("Correct: " + correct);
-        console.log(correct);
+    
         
     }
     else if (q1 == false) {
@@ -57,12 +60,12 @@ var answers = function () {
         $("#incorrect").html("Incorrect: " + incorrect);
     }
 
-    var q2 = $("#q2:input:radio:checked").val();
+    var q2 = $("input[name=Q2]:checked").val();
+    console.log("Q2", q2);
 
     if (q2 === "robert") {
         correct++;
         $("#correct").text("Correct: " + correct);
-        console.log(correct);
         
     }
     else if (q2 == false) {
@@ -75,12 +78,12 @@ var answers = function () {
         $("#incorrect").html("Incorrect: " + incorrect);
     }
 
-    var q3 = $("#q3:input:radio:checked").val();
+    var q3 = $("input[name=Q3]:checked").val();
+    console.log("Q3", q3);
 
     if (q3 === "drogon") {
         correct++;
         $("#correct").text("Correct: " + correct);
-        console.log(correct);
         
     }
     else if (q3 == false) {
@@ -93,12 +96,12 @@ var answers = function () {
         $("#incorrect").html("Incorrect: " + incorrect);
     }
 
-    var q4 = $("#q4:input:radio:checked").val();
+    var q4 = $("input[name=Q4]:checked").val();
+    console.log("Q4", q4);
 
     if (q4 === "barristan") {
         correct++;
         $("#correct").text("Correct: " + correct);
-        console.log(correct);
         
     }
     else if (q4 == false) {
@@ -111,12 +114,12 @@ var answers = function () {
         $("#incorrect").html("Incorrect: " + incorrect);
     }
 
-    var q5 = $("#q5:input:radio:checked").val();
+    var q5 = $("input[name=Q5]:checked").val();
+    console.log("Q5", q5);
 
     if (q5 === "olenna") {
         correct++;
         $("#correct").text("Correct: " + correct);
-        console.log(correct);
         
     }
     else if (q5 == false) {
